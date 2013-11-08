5122e0876cc19474fee62f13fc1650e407bd3d96
jackson.gilman <jackson.gilman@memphistn.gov>
2013-11-08 16:17:00 -0600

    removed reference to App in adapter.

--- arcgis_adapter.js
+++ arcgis_adapter.js
@@ -1,6 +1,32 @@
 DS.ArcGisAdapter = DS.RESTAdapter.extend({
   serializer: DS.ArcGisSerializer,
 
+  /**
+   * Retrieve the url suffix for a given type.
+   * @param {DS.Model} type - type for the record.
+   * @param [id] - id of the record the url is needed for
+   * @returns {String}
+   */
+  serviceUrlForType: function (type, id) {
+    // TODO: We cannot assume the application's name is `App`. [JJG 11/8/13]
+    var App = window.App,
+        name = this.capitalize(type),
+        url = window.App ? App[name] : name,
+        mapping;
+
+    if (url && url.hasOwnProperty("serviceURL")) {
+      mapping = url.serviceURL;
+    } else {
+      mapping = this.serviceMapping[url];
+    }
+
+    url = typeof mapping === "function" ? mapping(url, id) : mapping;
+    Ember.assert("No serviceURL defined for type %@".fmt(name), !url && url !== "");
+    return url;
+  },
+
+  serviceMapping: {},
+
   ajax: function (url, type, hash) {
     var adapter = this;
 
@@ -18,7 +44,6 @@ DS.ArcGisAdapter = DS.RESTAdapter.extend({
 
       hash.success = function (json) {
         var transformedJSON = adapter.transformJSON(json, hash.root);
-
         Ember.run(null, resolve, transformedJSON);
       };
 
@@ -30,29 +55,33 @@ DS.ArcGisAdapter = DS.RESTAdapter.extend({
     });
   },
 
-  buildURL: function (record) {
+  buildURL: function (type, id) {
     var baseUrl = this.get("url"),
-      namespace = this.get("namespace"),
-      serviceURL = App[this.capitalize(record)].serviceURL,
-      url = [baseUrl, namespace, serviceURL];
+        namespace = this.get("namespace"),
+
+    // Propose using some sort of url mapping property? [JJG 11/8/13]
+        serviceURL = this.serviceUrlForType(type, id),
+        url = [baseUrl, namespace, serviceURL];
 
     return url.join("/");
   },
 
   findQuery: function (store, type, query, recordArray) {
     var root = this.rootForType(type),
-      adapter = this,
-      rejectionHandler = function (reason) {
-        Ember.Logger.error(reason, reason.message);
-        throw reason;
-      };
+        adapter = this,
+        rejectionHandler = function (reason) {
+          Ember.Logger.error(reason, reason.message);
+          throw reason;
+        };
 
     return this.ajax(this.buildURL(root), "GET", {
       data: query,
       root: root
-    }).then(function(json){
-      adapter.didFindQuery(store, type, json, recordArray);
-    }).then(null, rejectionHandler);
+    })
+        .then(function (json) {
+          adapter.didFindQuery(store, type, json, recordArray);
+        })
+        .then(null, rejectionHandler);
   },
 
   capitalize: function (string) {
@@ -61,19 +90,19 @@ DS.ArcGisAdapter = DS.RESTAdapter.extend({
 
   transformJSON: function (json, root) {
     var feature,
-      index = 0,
-      pluralizedRoot = this.pluralize(root),
-      transformedJSON = {};
+        index = 0,
+        pluralizedRoot = this.pluralize(root),
+        transformedJSON = {};
 
     transformedJSON[pluralizedRoot] = [];
 
-    for(;index < json.features.length; index++) {
+    for (; index < json.features.length; index++) {
       feature = json.features[index];
 
-      if(feature.geometry) {
+      if (feature.geometry) {
         feature.attributes.geometry = feature.geometry;
       }
-      
+
       transformedJSON[pluralizedRoot].push(feature.attributes);
     }
 
